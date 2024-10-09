965c3b55500548f67840b5baca8f1e4b28b265d6
Riddle-of-eleven <nikavorosh@gmail.com>
2024-10-09 12:43:43 +0300

    Внесены изменения

--- get_area.py
+++ get_area.py
@@ -4,7 +4,7 @@ def get_nontransparent_area(image_name, image_format):
     image = Image.open(f'images/{image_name}.{image_format}')
     if image.mode == 'RGBA':
         # getdata возвращает содержимое изображения в виде набора объектов
-        nontransparent_pixels = sum(1 for pixel in image.getdata() if pixel[3] == 255)
+        nontransparent_pixels = sum(1 for pixel in image.getdata() if pixel[3] != 0)
         return nontransparent_pixels
     else:
         return image.width * image.height
